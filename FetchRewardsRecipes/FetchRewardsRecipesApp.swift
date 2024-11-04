b4de16cf0f5f122dba064cce1f0b44a52944ce7c
Luyuan Nathan <luyuan.nathan@gmail.com>
2024-11-04 13:31:30 -0500

    Initial commit

---
+++ FetchRewardsRecipes/FetchRewardsRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRewardsRecipesApp.swift
+//  FetchRewardsRecipes
+//
+//  Created by Luyuan Nathan on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRewardsRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRewardsRecipesTests/FetchRewardsRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRewardsRecipesTests.swift
+//  FetchRewardsRecipesTests
+//
+//  Created by Luyuan Nathan on 10/31/24.
+//
+
+import Testing
+@testable import FetchRewardsRecipes
+
+struct FetchRewardsRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
